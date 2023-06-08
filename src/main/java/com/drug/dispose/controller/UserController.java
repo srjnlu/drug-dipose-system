58b6a379f323f12aa4b50bb7f367f632a1ceef7f
Abhishek Singh6 <abhishek_singh6@epam.com>
2023-06-08 23:50:19 +0530

    logger implementation statrted

--- src/main/java/com/drug/dispose/controller/UserController.java
+++ src/main/java/com/drug/dispose/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.drug.dispose.controller;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,8 @@ import com.drug.dispose.dto.UserDTO;
 import com.drug.dispose.service.UserService;
 import com.drug.dispose.util.UserValidation;
 
+import ch.qos.logback.classic.Logger;
+
 @RestController
 @RequestMapping("/v1/user")
 public class UserController {
@@ -21,23 +24,34 @@ public class UserController {
 
 	@Autowired
 	private UserValidation userValidation;
+	
+//	@Autowired
+//	org.slf4j.Logger logger = LoggerFactory.getLogger(UserController.class);
 
 	@PostMapping("/register")
 	public ResponseEntity<?> createUser(@RequestBody UserDTO user) {
 
 		try {
 			if (userValidation.userNullCheck(user)) {
+				
+				 logger.trace("A TRACE Message");
+			        logger.debug("A DEBUG Message");
+			        logger.info("An INFO Message");
+			        logger.warn("A WARN Message");
+			        logger.error("An ERROR Message");
+
 				boolean isUserRegistered = userService.registerUser(user);
 				if (isUserRegistered) {
 					return new ResponseEntity<>(HttpStatus.CREATED);
 				} else {
-					return new ResponseEntity<>("Something went wrong while registering you", HttpStatus.BAD_GATEWAY);
+					return new ResponseEntity<>("Something went wrong while registering you..", HttpStatus.BAD_GATEWAY);
 				}
 			} else {
 				return new ResponseEntity<>("Please fill all the field", HttpStatus.BAD_REQUEST);
 			}
 
 		} catch (Exception e) {
+
 			return new ResponseEntity<>("Something went wrong", HttpStatus.BAD_GATEWAY);
 		}
 
